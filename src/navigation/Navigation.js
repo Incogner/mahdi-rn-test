ce1df840d682cda5ac8e483b67faff1a5b84ded0
Mahdi Moradi <mahdimoradi.1@gmail.com>
2021-05-20 16:10:04 -0400

    Update navigation for auth users

--- src/navigation/Navigation.js
+++ src/navigation/Navigation.js
@@ -2,23 +2,52 @@
 import { NavigationContainer } from '@react-navigation/native';
 import { createStackNavigator } from '@react-navigation/stack';
 import React from 'react';
+import { useSelector } from 'react-redux';
 
 import LoginScreen from '../screens/LoginScreen';
 import RegistrationScreen from '../screens/RegistrationScreen';
 import ShiftListScreen from '../screens/ShiftListScreen';
 
 export default Navigation = () => {
-
-    const Stack = createStackNavigator();
+    const isAuthenticated = useSelector(state => state.reducer.token !== null);
 
     return (
         <NavigationContainer>
+            <RootNavigator isAuth={isAuthenticated} />
+        </NavigationContainer>
+    );
+}
+
+const Stack = createStackNavigator();
+const RootNavigator = ({ isAuth }) => {
+    //console.log(isAuth);
+    return (
+        <Stack.Navigator screenOptions={{ headerShown: false }}>
+            {!isAuth ? (
+                // No token found, user isn't signed in
+                <Stack.Screen
+                    name="Login"
+                    component={LoginScreen}
+                    options={{
+                        title: 'Login',
+                        animationTypeForReplace: isAuth ? 'pop' : 'push',
+                    }}
+                />
+            ) : (
+                // User is signed in
+                <Stack.Screen name="ShiftList" component={ShiftListScreen} />
+            )}
+        </Stack.Navigator>
+    );
+}
+
+
+/*
+<NavigationContainer>
             <Stack.Navigator>
                 <Stack.Screen name="Login" component={LoginScreen} />
                 <Stack.Screen name="Registration" component={RegistrationScreen} />
                 <Stack.Screen name="ShiftList" component={ShiftListScreen} />
             </Stack.Navigator>
         </NavigationContainer>
-    );
-}
-
+*/
--- src/screens/RegistrationScreen.js
+++ src/screens/RegistrationScreen.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { View, Text } from 'react-native';
+import { View, Text, StyleSheet } from 'react-native';
 
 export default RegistrationScreen = () => {
     return (
@@ -8,3 +8,11 @@ export default RegistrationScreen = () => {
         </View>
     );
 }
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        justifyContent: 'center',
+        alignItems: 'center',
+    },
+});
