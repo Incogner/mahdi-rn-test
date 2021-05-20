e275e2a21d66f29bd8dd998b4478690116e5a94c
Mahdi Moradi <mahdimoradi.1@gmail.com>
2021-05-20 14:03:22 -0400

    Add simple screens

---
+++ src/screens/ShiftListScreen.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import { View, Text } from 'react-native';
+
+export default ShiftListScreen = () => {
+    return (
+        <View>
+            <Text>Shift List</Text>
+        </View>
+    );
+}
