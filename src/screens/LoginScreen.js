35e22d1af3ebb44992d635da4db1a650bb073b4d
Mahdi Moradi <mahdimoradi.1@gmail.com>
2021-05-20 15:06:17 -0400

    Include login states and handlers

--- src/screens/LoginScreen.js
+++ src/screens/LoginScreen.js
@@ -1,12 +1,45 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { View, Text, StyleSheet, TextInput, TouchableOpacity } from 'react-native';
+import updateObject from '../utility/utility';
 
 export default LoginScreen = () => {
+    const initialstate = {
+        controls: {
+          email: {
+            placeholder: 'Mail Address',
+            value: '',
+            validation: {
+              required: true,
+              isEmail: true
+            }
+          },
+          password: {
+            placeholder: 'Password',
+            value: '',
+            validation: {
+              required: true,
+              minLength: 6
+            }
+          }
+        }
+      }
+
+      const [data, setData] = useState(initialstate);
+
+      inputChangedHandler = (val, controlName) => {
+        const updatedControls = updateObject(data.controls, {
+          [controlName]: updateObject(data.controls[controlName], {
+            value: val
+          })
+        });
+        setData({ controls: updatedControls });
+      }
+
     return (
         <View style={styles.container}>
             <Text>Login Here</Text>
             <TextInput
-                placeholder="Email"
+                placeholder={data.controls.email.placeholder}
                 placeholderTextColor="#666666"
                 style={[styles.textInput, {
                     color: 'black'
@@ -14,7 +47,7 @@ export default LoginScreen = () => {
                 autoCapitalize="none"
             />
             <TextInput
-                placeholder="Password"
+                placeholder={data.controls.password.placeholder}
                 placeholderTextColor="#666666"
                 style={[styles.textInput, {
                     color: 'black'
